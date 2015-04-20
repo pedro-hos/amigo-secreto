81bfa7c845aa721070a259e360bbf5c0943eab60
Pedro Hos <pedro-hos@outlook.com>
2015-04-20 15:05:38 -0300

    parte do sorteio completo. Agora falta apenas enviar email e fazer uns testes unitarios

---
+++ server/src/main/java/br/com/amigo/secreto/controller/SorteioController.java
@@ -0,0 +1,94 @@
+package br.com.amigo.secreto.controller;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.amigo.secreto.model.entities.usuario.Usuario;
+import br.com.amigo.secreto.model.service.usuario.Usuarios;
+
+@RestController
+@RequestMapping(value="api/sorteio")
+public class SorteioController {
+	
+	@Autowired Usuarios usuarios;
+	
+	@RequestMapping( method = GET, produces = APPLICATION_JSON_VALUE )
+	public ResponseEntity<List<Usuario>> sortear() {
+		
+		List<Usuario> todos = usuarios.todos();
+		
+		if( todos.isEmpty() ) {
+			return new ResponseEntity<List<Usuario>>(NOT_FOUND);
+		}
+		
+		sortearAmigos(todos);
+		
+		//Vamos armezenar quem tirou quem! \o
+		usuarios.salveAll(todos);
+		
+		return new ResponseEntity<List<Usuario>>( todos, OK );
+	}
+	
+	/**
+	 * Método que realiza o sorteio, tomando cuidado para que a 
+	 * pessoal não tire ela mesma e que não tirem ela mesma
+	 */
+	protected void sortearAmigos(List<Usuario> todos) {
+		
+		int indice = 0;
+		List<Integer> jaSorteados = new ArrayList<Integer>();
+		List<Usuario> auxiliar = new ArrayList<Usuario>(todos);
+		
+		for (Usuario usuario : todos) {
+			
+			/*
+			 * Um cuidado extra para não dar caquinha por conta da recurvidade em
+			 * retornaIndice(); Se ele for o ultimo basta pegar que esta sobrando
+			 * na lista auxiliar.
+			 */
+			if(jaSorteados.size() == todos.size() - 1) {
+				usuario.setAmigoSecreto(auxiliar.get(0));
+				continue;
+			}
+			
+			indice = retornaIndice(todos.indexOf(usuario), todos.size(), jaSorteados);
+			jaSorteados.add(indice);
+			auxiliar.remove(todos.get(indice));
+			
+			usuario.setAmigoSecreto(todos.get(indice));
+		}
+		
+	}
+	
+	/**
+	 * Método que retira o índice para pegar na lista
+	 */
+	protected int retornaIndice( int indiceUsuarioNaLista, 
+								 int maxParaSorteio, 
+								 List<Integer> jaSorteados ) {
+		
+		Random random = new Random();
+		int indice = random.nextInt(maxParaSorteio);
+		
+		if(indice == indiceUsuarioNaLista){
+			return retornaIndice(indiceUsuarioNaLista, maxParaSorteio, jaSorteados);
+		} else if (jaSorteados.contains(indice)) {
+			return retornaIndice(indiceUsuarioNaLista, maxParaSorteio, jaSorteados);
+		}
+		
+		return indice;
+		
+	}
+	
+}
